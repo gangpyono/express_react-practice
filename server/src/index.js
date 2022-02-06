190e8f57c639c9288bf7c52a4411a10d3acc6fcc
shrkdvy <shrkdvy123@gmail.com>
2022-02-07 01:17:57 +0900

    [chore] : resolvers,schema 설정, apollo 3.0 업그레이드에따른 재설정

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,9 @@
 // express 앱을 띄우기위한 기능들 정의
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
-import cors from "cors";
-
-const app = express();
-
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+import resolvers from "./resolvers/index.js";
+import schema from "./schema/index.js";
+import { readDB } from "./dbController.js";
 
 //rest api 서버는 사용자가 요청한 route에 따라 그에 대응하는 res를 응답값으로 보내주는 형태.
 //graphql 서버는 graphql이라는 하나의 path로 요청값을받아 판단후 res로 응답한다. (route가 없다.) "/graphql"
@@ -27,7 +20,16 @@ const server = new ApolloServer({
   },
 });
 
+const app = express();
+await server.start();
 server.applyMiddleware({
   app,
   path: "/graphql",
+  cors: {
+    origin: ["http://localhost:3000", "https://studio.apollographql.com"],
+    credentials: true,
+  },
 });
+
+await app.listen({ port: 8000 });
+console.log("server listening on 8000...");
