bd9696ceab5bda44b9b1f6eb7667cda3d15970e3
shrkdvy <shrkdvy123@gmail.com>
2022-02-06 23:22:51 +0900

    [edit] : root inext.js GraphQL로 변환

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,9 @@
 // express 앱을 띄우기위한 기능들 정의
-
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
 import cors from "cors";
-import messagesRoute from "./routes/messages.js";
-import usersRoute from "./routes/users.js";
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); // josn을 사용하겠다.
 
 app.use(
   cors({
@@ -16,18 +12,22 @@ app.use(
   })
 );
 
-//app[method](route,handler) 형식
-// app.get("/", (req, res) => {
-//   res.send("ok"); // '/' 라우트에서 get메서드 요청시 실행됨.
-// });
+//rest api 서버는 사용자가 요청한 route에 따라 그에 대응하는 res를 응답값으로 보내주는 형태.
+//graphql 서버는 graphql이라는 하나의 path로 요청값을받아 판단후 res로 응답한다. (route가 없다.) "/graphql"
 
-const routes = [...messagesRoute, ...usersRoute];
-// 이벤트 핸들러와같이 감시자등록으로 생각.
-
-routes.forEach(({ method, route, handler }) => {
-  app[method](route, handler);
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  context: {
+    db: {
+      // resolvers에 의해 읽혀질 DB
+      messages: readDB("messages"),
+      users: readDB("users"),
+    },
+  },
 });
 
-app.listen(8000, () => {
-  console.log("server listening on 8000...");
+server.applyMiddleware({
+  app,
+  path: "/graphql",
 });
