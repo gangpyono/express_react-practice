533eda9280a0733fbc2a834ee9b281610c11209b
shrkdvy <shrkdvy123@gmail.com>
2022-02-12 20:17:35 +0900

    [edit] :client CRUD rest -> graphql 변경

--- client/components/MsgList.js
+++ client/components/MsgList.js
@@ -1,61 +1,100 @@
 import { useEffect, useState, useRef } from "react";
-
 import { useRouter } from "next/router";
-
+import { useQueryClient, useMutation, useQuery } from "react-query";
 import MsgItem from "./MsgItem";
 import MsgInput from "./MsgInput";
-import fetcher from "../fetcher";
-import useInfiniteScroll from "../hooks/useInfiniteScroll";
+import { fetcher, QueryKeys } from "../queryClient";
+import {
+  GET_MESSAGES,
+  CREATE_MESSAGE,
+  UPDATE_MESSAGE,
+  DELETE_MESSAGE,
+} from "../graphql/message";
+
+// import useInfiniteScroll from "../hooks/useInfiniteScroll";
 
 const MsgList = ({ smsgs, users }) => {
+  const client = useQueryClient();
   const { query } = useRouter();
   const userId = query.userId || query.userid || ""; // url을 무조건 소문자로 치환해 버리는 경우에대한 대응.
   // const { userId } = query;
   const [msgs, setMsgs] = useState(smsgs);
   const [editingId, setEditingId] = useState(null);
-  const [hasNext, setHasNext] = useState(true); // 무한스크롤시 모든 데이터를 불러왔을때 반복요청을 막기위함
-  const fetchMoreEl = useRef(null); // 무한스크롤시 판변기준
-  const intersecting = useInfiniteScroll(fetchMoreEl); // fecthMoreEl가 화면에노출되면 true, 아니면 false
+  // const [hasNext, setHasNext] = useState(true); // 무한스크롤시 모든 데이터를 불러왔을때 반복요청을 막기위함
+  // const fetchMoreEl = useRef(null); // 무한스크롤시 판변기준
+  // const intersecting = useInfiniteScroll(fetchMoreEl); // fecthMoreEl가 화면에노출되면 true, 아니면 false
 
-  const onCreate = async (text) => {
-    const newMsg = await fetcher("post", "/messages", { text, userId });
-    if (!newMsg) throw Error("something wrong");
-    setMsgs((msgs) => [newMsg, ...msgs]);
-  };
+  const { mutate: onCreate } = useMutation(
+    ({ text }) => fetcher(CREATE_MESSAGE, { text, userId }),
+    {
+      onSuccess: ({ createMessage }) => {
+        client.setQueryData(QueryKeys.MESSAGES, (old) => {
+          return {
+            // messages -> 서버의 schema에서 작성된 변수
+            messages: [createMessage, ...old.messages],
+          };
+        });
+      },
+    }
+  );
 
-  const onUpdate = async (text, id) => {
-    const newMsg = await fetcher("put", `/messages/${id}`, { text, userId });
+  const { mutate: onUpdate } = useMutation(
+    ({ text, id }) => fetcher(UPDATE_MESSAGE, { text, id, userId }),
+    {
+      onSuccess: ({ updateMessage }) => {
+        client.setQueryData(QueryKeys.MESSAGES, (old) => {
+          const targetIndex = old.messages.findIndex(
+            (msg) => msg.id === updateMessage.id
+          );
+          if (targetIndex < 0) return old;
+          const newMsgs = [...old.messages];
+          newMsgs.splice(targetIndex, 1, updateMessage);
+          return { messages: newMsgs };
+        });
+        doneEdit();
+      },
+    }
+  );
 
-    if (!newMsg) throw Error("something wrong");
-
-    setMsgs((msgs) => {
-      const targetIndex = msgs.findIndex((msg) => msg.id === id);
-      if (targetIndex < 0) return msgs;
-      const newMsgs = [...msgs];
-      newMsgs.splice(targetIndex, 1, newMsg);
-      return newMsgs;
-    });
-
-    doneEdit();
-  };
-
-  const onDelete = async (id) => {
-    const receviedId = await fetcher("delete", `/messages/${id}`, {
-      params: { userId },
-    });
-
-    if (!receviedId) throw Error("something wrong");
-
-    setMsgs((msgs) => {
-      const targetIndex = msgs.findIndex((msg) => msg.id === receviedId + ""); // 기존에 있던 숫자형으로 작성된 id를 타입떄문에 처리해주지 못함.
-      if (targetIndex < 0) return msgs;
-      const newMsgs = [...msgs];
-      newMsgs.splice(targetIndex, 1);
-      return newMsgs;
-    });
-  };
+  const { mutate: onDelete } = useMutation(
+    (id) => fetcher(DELETE_MESSAGE, { id, userId }),
+    {
+      onSuccess: ({ deleteMessage: deletedId }) => {
+        client.setQueryData(QueryKeys.MESSAGES, (old) => {
+          const targetIndex = old.messages.findIndex(
+            (msg) => msg.id === deletedId
+          );
+          if (targetIndex < 0) return old;
+          const newMsgs = [...old.messages];
+          newMsgs.splice(targetIndex, 1);
+          return { messages: newMsgs };
+        });
+      },
+    }
+  );
 
   const doneEdit = () => setEditingId(null);
+
+  // useQuery 정의로 들어가서 제공하는 여러 값들을 확인 할 수 있다.
+  // query를 날리는 방식 3가지.(정의에서 확인)
+  // graphql방식에선 query key가 있어야 한다.
+  // query key가 같은 쿼리는 값을 공유할 수 있다.
+
+  const { data, error, isError } = useQuery(QueryKeys.MESSAGES, () =>
+    //stale를 먼저 사용한다(cache데이터). 그후 서버로부터 받은 데이터와 비교하여 값이 다르다면 데이터를 덮어씌운다.  //stale : 옛것 . 미리 받아놓은 정보.(클라이언트에있는 데이터)
+    fetcher(GET_MESSAGES)
+  );
+
+  useEffect(() => {
+    if (!data?.messages) return;
+    console.log("msg changed");
+    setMsgs(data?.messages);
+  }, [data?.messages]);
+
+  if (isError) {
+    console.log(error);
+    return null;
+  }
 
   const getMessages = async () => {
     const newMsgs = await fetcher("get", "/messages", {
@@ -75,9 +114,9 @@ const MsgList = ({ smsgs, users }) => {
   //   getMessages();
   // }, []);
 
-  useEffect(() => {
-    if (intersecting && hasNext) getMessages(); // 최초에는 데이터가 없는상태이기떄문에 무조건 true가 될것이다. 따라서 바로 윗줄의 useEffect와 중복된 요청을 보내게된다.
-  }, [intersecting]);
+  // useEffect(() => {
+  //   if (intersecting && hasNext) getMessages(); // 최초에는 데이터가 없는상태이기떄문에 무조건 true가 될것이다. 따라서 바로 윗줄의 useEffect와 중복된 요청을 보내게된다.
+  // }, [intersecting]);
 
   return (
     <>
@@ -93,11 +132,11 @@ const MsgList = ({ smsgs, users }) => {
             startEdit={() => setEditingId(x.id)}
             isEditing={editingId === x.id}
             myId={userId}
-            user={users[x.userId]}
+            user={users.find((x) => x.userId === x.userId)}
           />
         ))}
       </ul>
-      <div ref={fetchMoreEl} />
+      {/* <div ref={fetchMoreEl} /> */}
     </>
   );
 };
